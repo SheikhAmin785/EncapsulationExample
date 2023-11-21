279748c715c71371d4df9da9fde29e1feb82d962
SheikhAmin785 <shakil785ks@gmail.com>
2023-11-21 21:34:09 +0600

    Add project files.

---
+++ Program1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program1
+{
+    static List<List<int>> g = new List<List<int>>();
+    static bool[] vis;
+
+    static void DFS(int vertex)
+    {
+        Console.WriteLine(vertex);
+
+        vis[vertex] = true;
+
+        foreach (int child in g[vertex])//2,3
+        {
+            Console.WriteLine($"parent {vertex}, child {child}");
+            if (!vis[child])
+            {
+                DFS(child);
+            }
+        }
+    }
+
+    static void Main()
+    {
+        const int N = 100010; // 1e5 + 10
+        vis = new bool[N];
+
+        int n, m;
+        string[] input = Console.ReadLine().Split();
+        n = int.Parse(input[0]);
+        m = int.Parse(input[1]);
+
+        for (int i = 0; i <= n; i++)
+        {
+            g.Add(new List<int>());
+            vis[i] = false;
+        }
+
+        for (int i = 0; i < m; i++)
+        {
+            input = Console.ReadLine().Split();
+            int v1 = int.Parse(input[0]);
+            int v2 = int.Parse(input[1]);
+
+            g[v1].Add(v2);
+            g[v2].Add(v1);
+        }
+
+        DFS(1);
+    }
+}
+
---
+++ Rectangle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationExample
+{
+    class Rectangle
+    {
+        private int width;
+        private int height;
+
+         public void Area()
+        {
+            Console.WriteLine("enter your width");
+            width = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter your height");
+            height = Convert.ToInt32(Console.ReadLine());
+        }
+        public void Logic()
+        {
+            return height * width;
+        }
+        public void Display()
+        {
+            Console.WriteLine("length:{0}",height);
+            Console.WriteLine("width:{0}", width);
+            Console.WriteLine("area:{0}", Logic());
+        }
+
+
+       
+    }
+    class ExecuteRectangle
+    {
+        static void Main()
+        {
+            Rectangle obj = new Rectangle();
+            obj.Area();
+            obj.Display();
+            Console.ReadLine();
+
+        }
+    }
+}
